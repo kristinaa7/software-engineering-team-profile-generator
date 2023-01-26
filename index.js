7ccab0b50d907563e03a31121459ed40f5b3b204
Kristina Alabastro <kristinaa7@live.com>
2023-01-25 23:23:15 -0800

    added prompts about team and positions

--- index.js
+++ index.js
@@ -2,3 +2,62 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 const reset = require('./reset.js')
+
+const prompts = [
+    {
+        type: 'input',
+        message: "What is the team manager's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's id?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's email?",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's office number?",
+        name: 'officenumber',
+        number: >0,
+    },
+    {
+        type: 'input',
+        message: "What type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+        default: 'Engineer',
+        name: 'position1',
+    },
+    {
+        type: 'input',
+        message: "What is your ${position}'s name?",
+        name: 'pname',
+    },
+    {
+        type: 'input',
+        message: "What is your ${position}'s id?",
+        name: 'pie',
+    },
+    {
+        type: 'input',
+        message: "What is your ${position}'s email?",
+        name: 'pemail',
+    },
+    {
+        type: 'input',
+        message: "What is your ${position}'s school?",
+        name: 'pschool',
+    },
+    {
+        type: 'list',
+        message: "What type of team member would you like to add?",
+        name: 'position2',
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+        default: 'Engineer',
+    },
+]
+

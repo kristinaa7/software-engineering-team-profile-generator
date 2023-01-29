400462cbd427b8020f69e6ee2816ed27cd603282
Kristina Alabastro <kristinaa7@live.com>
2023-01-29 00:13:58 -0800

    added specific individuals to team html file

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,11 @@
 const Manager = require('./lib/Manager')
 
-const manager = new Manager('Kris Smith', '199712', 'kristina@gmail.com', '1234567890');
+const manager = new Manager('Kris Smith', '199712', 'kris@gmail.com', '1234567890');
 
 describe ('creates a manager object',() => {
     expect(manager.name).toEqual('Kris Smith');
     expect(manager.id).toEqual('199712');
-    expect(manager.email).toEqual('kristina@gmail.com');
+    expect(manager.email).toEqual('kris@gmail.com');
     expect(manager.officenumber).toEqual('1234567890');
 });
 

82566cabef1653cce4fd81428afe54e27a86d287
Kristina Alabastro <kristinaa7@live.com>
2023-01-28 23:46:58 -0800

    added employee role prompts in index.js

--- index.js
+++ index.js
@@ -1,76 +1,131 @@
 //user needs to install node version 16 and inquirer package by typing npm install in the terminal to work
 const inquirer = require('inquirer')
 const fs = require('fs')
-const generateMarkdown = require('./generateMarkdown')
+const generateMarkdown = require('./src/generateMarkdown')
 
 //position classes are imported
-const Manager = require('./Manager')
-const Engineer = require('./Engineer')
-const Intern = require('./Intern')
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
 
 //Array for answers to questions
-const Employee = [];
+const employee = [];
 
-const questions = [
+const managerQuestions = () => {
+    inquirer.prompt ([
     {
         type: 'input',
-        message: "What is the team manager's name?",
-        name: 'managerName',
+        message: `Welcome to the team generator!
+        Use 'npm run reset' to reset the dist/ folder
+        
+        Please build your team.
+        What is your team manager's name?`,
+        name: 'name',
     },
     {
         type: 'input',
         message: "What is the team manager's id?",
-        name: 'managerId',
+        name: 'id',
     },
     {
         type: 'input',
         message: "What is the team manager's email?",
-        name: 'managerEmail',
+        name: 'email',
     },
     {
         type: 'input',
         message: "What is the team manager's office number?",
-        name: 'managerOffice',
+        name: 'officeNumber',
     },
+])
+.then(answers => {
+    const manager = new Manager (answers.name, answers.id, answers.email, answers.officeNumber);
+    employee.push(manager)
+    addEmployee();
+})};
+
+const addEmployee = () => {
+    inquirer.prompt([
     {
         type: 'list',
         message: "What type of team member would you like to add?",
         choices: ["Engineer", "Intern", "I don't want to add any more team members"],
         default: 'Engineer',
-        name: 'employeeType',
+        name: 'addEmployeeType',
+    }])
+
+.then(function (userInput){
+    switch(userInput.addEmployeeType) {
+        case "Engineer":
+        addEngineer();
+        break;
+        case "Intern":
+        addIntern();
+        break;
+
+        default:
+            createHTML();
+    }
+})
+};
+
+const addEngineer = () => {
+   inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'engineerName',
     },
     {
         type: 'input',
-        message: "What is your ${position}'s name?",
-        name: 'pname',
+        message: "What is the engineer's ID?",
+        name: 'engineerID',
     },
     {
         type: 'input',
-        message: "What is your ${position}'s id?",
-        name: 'pie',
+        message: "What is the engineer's email?",
+        name: 'engineerEmail',
     },
     {
         type: 'input',
-        message: "What is your ${position}'s email?",
-        name: 'pemail',
+        message: "What is the engineer's Github?",
+        name: 'engineerGithub',
+    },
+
+])
+
+.then(answers => {
+    const engineer = new Engineer (answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+    employee.push(engineer);
+    addEmployee();
+})};
+
+const addIntern = () => {
+    inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is your name?",
+        name: 'internName',
     },
     {
         type: 'input',
-        message: "What is your ${position}'s school?",
-        name: 'pschool',
+        message: "What is your ID?",
+        name: 'internID',
     },
     {
-        type: 'list',
-        message: "What type of team member would you like to add?",
-        name: 'position2',
-        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-        default: 'Engineer',
+        type: 'input',
+        message: "What is your email?",
+        name: 'internEmail',
     },
-]
+    {
+        type: 'input',
+        message: "What university did you attend?",
+        name: 'school',
+    },
+])
 
-const HTML = generateMarkdown(data);
-fs.writeFileSync("team-profile.html", HTML, (err) => {})
-
-
-//loop for 
-
+.then(answers => {
+    const intern = new Intern (answers.internName, answers.internID, answers.internEmail, answers.school);
+    employee.push(intern);
+    addEmployee();
+})};

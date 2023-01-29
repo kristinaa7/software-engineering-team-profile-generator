875c8160e95b43b3b2516384d16c65027caedccf
Kristina Alabastro <kristinaa7@live.com>
2023-01-29 00:02:35 -0800

    added test code to engineertest file

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,17 @@
 const Engineer = require('./lib/Engineer')
+const engineer = new Engineer('Jordan Smith', '123456', 'jordan@gmail.com');
+
+describe ('creates an engineer object',() => {
+    expect(engineer.name).toEqual('Jordan Smith');
+    expect(engineer.id).toEqual('123456');
+    expect(engineer.email).toEqual('jordan@gmail.com');
+    expect(engineer.github).toEqual('jordangit');
+});
+
+describe('return engineer github',() => {
+    expect(engineer.getGithub()).toEqual(expect.stringContaining('jordangit'));
+});
+
+describe('return engineer role',() => {
+    expect(engineer.getRole()).toEqual(expect.stringContaining('Engineer'));
+});

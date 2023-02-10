136ee4d22218615cbc29985f1ce1979ddee3587e
Kristina Alabastro <kristinaa7@live.com>
2023-02-10 15:23:37 -0800

    corrected intern test file renamed test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,17 +1,17 @@
 const Intern = require('../lib/Intern')
 const intern = new Intern('Eden Smith', '050923', 'eden@gmail.com', 'Washington');
 
-describe ('creates an intern object',() => {
+test ('creates an intern object',() => {
     expect(intern.getName()).toBe('Eden Smith');
     expect(intern.getId()).toBe('050923');
     expect(intern.getEmail()).toBe('eden@gmail.com');
     expect(intern.getSchool()).toBe('Washington');
 });
 
-describe('return intern school',() => {
+test ('return intern school',() => {
     expect(intern.getSchool()).toEqual(expect.stringContaining('Washington'));
 });
 
-describe('return intern role',() => {
-    expect(intern.getRole()).toEqual(expect.stringContaining('Engineer'));
+test ('return intern role',() => {
+    expect(intern.getRole()).toEqual(expect.stringContaining('Intern'));
 });

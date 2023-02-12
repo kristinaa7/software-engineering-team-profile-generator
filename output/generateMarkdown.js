9c5092a3dc52bbdf32d0c1aa9de91b815a1151cb
Kristina Alabastro <kristinaa7@live.com>
2023-02-12 08:03:12 -0800

    added 2 new functions generateEngineer and generateIntern;

--- output/generateMarkdown.js
+++ output/generateMarkdown.js
@@ -5,29 +5,6 @@ const generateTeam = team => {
 
     const generateManager = manager => {
         return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>My Team Profile</title>
-        <link rel="stylesheet" href="style.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    </head>
-        
-    <body>
-        <div class="jumbotron text-center">
-            <div class="row">
-                <div class="col-12 p-5 mb-3 team-heading">
-                    <h1 class="display-4">My Team</h1>
-                </div>
-            </div>
-        </div>
-    <div class="container-fluid text-center">
-        <div class="row">
-          <div class="team-area col-12 d-flex">
-            
     <div class="card employee-card">
         <div class="card-body card-header bg-primary text-white">
             <h2 class="card-title">${manager.getName()}</h2>
@@ -41,7 +18,10 @@ const generateTeam = team => {
             </ul>
         </div>
     </div>
-        
+    `};
+
+    const generateEngineer = engineer => {
+        return `
     <div class="card employee-card">
     <div class="card-body card-header bg-primary text-white">
             <h2 class="card-title">${engineer.getName()}</h2>
@@ -55,7 +35,10 @@ const generateTeam = team => {
             </ul>
             </div>
         </div>
-        
+    `};
+
+    const generateIntern = intern => {
+        return `
     <div class="card employee-card">
         <div class="card-header bg-primary text-white">
             <h2 class="card-title">${intern.getName()}</h2>
@@ -69,15 +52,10 @@ const generateTeam = team => {
         </ul>
         </div>
     </div>
-                </div>
-            </div>
-        </div>
-    </body>
-        
-    </html>`
-    };
+    `};
 
     let teamHTML = [];
+
     teamHTML.push(team
     .filter(mgr => mgr.getRole() === "Manager")
     .map(mgr => generateManager(mgr)));
@@ -93,4 +71,8 @@ const generateTeam = team => {
     return teamHTML.join("");
 };
 
-module.exports = generateTeam;
+module.exports = generateTeam => {
+    return `
+
+    
+    `};

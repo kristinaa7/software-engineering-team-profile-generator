49e9240b9ee3e2aa987c74f2abd09c08ab707065
Kristina Alabastro <kristinaa7@live.com>
2023-02-12 08:04:46 -0800

    exported the entire team to generate a html file

--- output/generateMarkdown.js
+++ output/generateMarkdown.js
@@ -73,6 +73,33 @@ const generateTeam = team => {
 
 module.exports = generateTeam => {
     return `
-
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team Profile</title>
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    
+    <body>
+       <div class="jumbotron text-center">
+            <div class="row">
+                <div class="col-12 p-5 mb-3 team-heading">
+                    <h1 class="display-4">My Team</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid text-center">
+            <div class="row">
+              <div class="team-area col-12 d-flex">
+              ${generateTeam(team)}
+              </div>
+            </div>
+          </div>
+      </body>
+      </html>
     
     `};
